c5ec61a362489158d8f81e9caabe59152bf3a048
LeviGoulart <levi.2006@alunos.utfpr.edu.br>
2025-05-15 16:45:08 -0300

    Add files via upload

---
+++ Exercício 2.da lisata.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int arredonda(float x){
+        int parte_inteira = (int)x;
+        float decimal = x - parte_inteira;
+
+        if (decimal >= 0.5 || decimal <= -0.5){
+            if (x > 0){
+                return parte_inteira + 1;
+            }else{
+                return parte_inteira - 1;
+            }
+        }
+        return parte_inteira;
+    }
+
+int main(){
+        float valor;
+
+        printf("Digite um valor para arredondar: ");
+        scanf("%f",&valor);
+
+        printf("Valor arredondado: %d\n", arredonda (valor));
+
+        return 0;
+}
